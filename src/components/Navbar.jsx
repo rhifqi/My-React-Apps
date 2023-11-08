d3cc4ba04e9a98b2d1bc15c70e5d30da2f0878d2
Rhifqi Gustyadi <122706917+rhifqi@users.noreply.github.com>
2023-11-08 14:48:22 +0700

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+     <div className="Navbar">
+       <h1> My Web</h1>
+        <div>
+        <p>Home</p>
+        <p>About Us</p>
+        <p>Gallery</p>
+
+     </div>
+    </div>
+    )
+}
+
+export default Navbar;
